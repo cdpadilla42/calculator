acb43938ae21e1e87c93b90e3aa8a01aa194500b
Chris Padilla <cdpadilla42@gmail.com>
2019-11-15 16:39:24 -0600

    Initiate style. Create calc grids, add spacing, center on page, add title.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // To-Do:
-// Next: EXTRA CREDIT: Add a “backspace” button, so the user can undo if they click the wrong number.
-//
+// Next: 
+// EXTRA CREDIT: Make it look nice! This can be a good portfolio project… but not if it’s UGLY. At least make the operations a different color from the keypad buttons.
 
 /*
-EXTRA CREDIT: Make it look nice! This can be a good portfolio project… but not if it’s UGLY. At least make the operations a different color from the keypad buttons.
 EXTRA EXTRA CREDIT: Refactor your code to store date in arrays as opposed to the DOM
 */
 
